56781a79736de58bace3f813bf89dd523e7a3b71
Jojo <jojo@example.com>
2016-12-02 12:37:17 +0100

    initial commit

---
+++ entrypoint.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Check requirements
+if [ "$LETSENCRYPT_EMAIL" == "none" ]; then echo "Please fill environment variable LETSENCRYPT_EMAIL"; exit; fi
+
+# Wait for set period
+echo "### Waiting for ${STARTUP_WAIT} seconds... "
+sleep ${STARTUP_WAIT}
+echo "Done"
+
+# Renew existing and expiring certificates if existing
+# if [ -d "/etc/letsencrypt/live" ]; then
+#  echo "### Renew existing and expiring certificates"
+#  letsencrypt renew --rsa-key-size $RSA_KEY_SIZE
+# fi
+
+# Generating each certificate
+i=1
+var=CERT1
+echo "### Generating certificates..."
+while [ "${!var}" ]
+do
+  # Get differents subdomains
+  SUBDOMAINS=$(echo ${!var} | tr ";" "\n")
+  SUBDOMAINS_ARGS=" "
+  for subdomain in $SUBDOMAINS
+  do
+    SUBDOMAINS_ARGS+="-d $subdomain "
+  done
+  echo "SUBDOMAINS_ARGS: $SUBDOMAINS_ARGS"
+
+  # Generate certificates
+  echo "Generate ${!var} certificate"
+  letsencrypt certonly  --text --non-interactive --rsa-key-size $RSA_KEY_SIZE \
+                    --email $LETSENCRYPT_EMAIL --agree-tos --standalone --expand \
+                    $SUBDOMAINS_ARGS -vvvvv --text
+
+  # Passing to the next certificate
+  i=$((i+1))
+  var="CERT$i"
+done
+
+# Launch reverse-proxy
+nginx
+
+# Nginx log
+tail -f /var/log/nginx/error.log
